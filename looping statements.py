1258ee54f26b1fda0e76b79d80169832ca66f9d4
MANDALAPU PUSHPANJALI <pushpanjali1614@gmail.com>
2026-07-10 16:19:57 +0530

    Add files via upload

---
+++ looping statements.py
@@ -0,0 +1,73 @@
+#print numbers 1-100,100-1,even numbers,and odd numbers
+#for i in range(1,101):
+#    print(i)
+
+
+#for i in range(100,0,-1):
+#    print(i)
+
+
+#for i in range(0,100,2):
+#    print(i)
+
+
+#for i in range(1,100,2):
+#    print(i)
+
+
+
+#Write a program to print numbers from 1 to 10 using a for loop.
+#Write a program to print even numbers between 1 and 20.
+#Write a program to print odd numbers from 1 to 15.
+#Write a program to print the multiplication table of 5.
+#Write a program to calculate the sum of numbers from 1 to 100.
+#Write a program to print numbers from 10 to 1 in reverse order.
+#Write a program to count how many numbers are there from 1 to 50.
+#Write a program to print the square of each number from 1 to 10.
+#Write a program to print all numbers between 1 and 30 that are divisible by 3.
+#Write a program to print the first 10 natural numbers.
+
+
+#for i in range(1,11):
+#    print(i)
+
+
+#for i in range(0,21,2):
+#    print(i)
+
+
+#for i in range(1,16,2):
+#    print(i)
+
+
+#a=int(input("enter a number:"))
+#for i in range(1,11):
+#    print(a*i)
+
+#sum=0
+#for i in range(1,101):
+#    sum = sum + i
+#print(sum)
+
+
+#for i in range(10,0,-1):
+#    print(i)
+
+
+#c=0
+#for i in range(1,51):
+#   c+=1
+#print(c)
+
+
+#for i in range(1,11):
+#    print(i*i)
+
+
+#for i in range(1,31):
+#    if i % 3==0:
+#        print(i)
+
+
+#for i in range(1,11):
+#    print(i)
